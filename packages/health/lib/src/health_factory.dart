f02f313d6836db9c15ea245905320d19c68d2efe
Tyler Lam <tylerl264@gmail.com>
2023-04-11 17:37:23 -0600

    feat: Add sleep stages

--- packages/health/lib/src/health_factory.dart
+++ packages/health/lib/src/health_factory.dart
@@ -241,6 +241,9 @@ class HealthFactory {
     if (type == HealthDataType.SLEEP_ASLEEP ||
         type == HealthDataType.SLEEP_AWAKE ||
         type == HealthDataType.SLEEP_IN_BED ||
+        type == HealthDataType.SLEEP_CORE ||
+        type == HealthDataType.SLEEP_DEEP ||
+        type == HealthDataType.SLEEP_REM ||
         type == HealthDataType.HEADACHE_NOT_PRESENT ||
         type == HealthDataType.HEADACHE_MILD ||
         type == HealthDataType.HEADACHE_MODERATE ||
@@ -496,6 +499,12 @@ class HealthFactory {
         return 1;
       case HealthDataType.SLEEP_AWAKE:
         return 2;
+      case HealthDataType.SLEEP_CORE:
+        return 3;
+      case HealthDataType.SLEEP_DEEP:
+        return 4;
+      case HealthDataType.SLEEP_REM:
+        return 5;
       case HealthDataType.HEADACHE_UNSPECIFIED:
         return 0;
       case HealthDataType.HEADACHE_NOT_PRESENT:
