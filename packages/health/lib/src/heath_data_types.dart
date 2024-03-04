62b85e9932c512b939bcd6fef63590dc8e1c6cd8
Kamalpreet Mundi <kamalmundi31@gmail.com>
2024-03-04 12:16:14 -0700

    Revert "tried to add android wheelchair access"
    This reverts commit 13014c7c3d123c1f84c7eaef3b3adf269f6151e0.

--- packages/health/lib/src/heath_data_types.dart
+++ packages/health/lib/src/heath_data_types.dart
@@ -312,7 +312,6 @@ const Map<HealthDataType, HealthDataUnit> dataTypeToUnit = {
   HealthDataType.RESTING_HEART_RATE: HealthDataUnit.BEATS_PER_MINUTE,
   HealthDataType.PUSH_COUNT: HealthDataUnit.COUNT,
   HealthDataType.STEPS: HealthDataUnit.COUNT,
-  HealthDataType.WHEELCHAIR_PUSHES: HealthDataUnit.COUNT
   HealthDataType.WAIST_CIRCUMFERENCE: HealthDataUnit.METER,
   HealthDataType.WALKING_HEART_RATE: HealthDataUnit.BEATS_PER_MINUTE,
   HealthDataType.WEIGHT: HealthDataUnit.KILOGRAM,
